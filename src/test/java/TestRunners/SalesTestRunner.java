412eb5b6afd58a9a34a82f381396a4fe1a95516b
SadiaAfsana <flora24488@gmail.com>
2022-01-28 22:38:49 +0600

    Update

--- src/test/java/TestRunners/SalesTestRunner.java
+++ src/test/java/TestRunners/SalesTestRunner.java
@@ -6,21 +6,21 @@ import Setup.Setup;
 import org.testng.annotations.Test;
 
 public class SalesTestRunner extends Setup {
-    @Test(priority = 0, enabled = false)
+    @Test(priority = 0,description = "Go to Sale Page")
     public void visitSaleTab() {
         driver.get("https://envothemes.com/envo-ecommerce");
         Sales sales = new Sales(driver);
         sales.doClickSalesTab();
     }
 
-    @Test(priority = 1, enabled = false)
+    @Test(priority = 1,description = "Check if all sales item have sale tag")
     public void checkSalesTag() {
         driver.get("https://envothemes.com/envo-ecommerce");
         Sales sales = new Sales(driver);
         sales.verifySalesTag();
     }
 
-    @Test(priority = 2)
+    @Test(priority = 2,description = "Select a Product")
     public void viewProduct() {
         driver.get("https://envothemes.com/envo-ecommerce");
         Sales sales = new Sales(driver);
