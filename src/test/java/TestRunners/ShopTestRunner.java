4441d8d622643020f824e0469af7586f7ec98117
SadiaAfsana <flora24488@gmail.com>
2022-01-28 22:11:07 +0600

    Update

--- src/test/java/TestRunners/ShopTestRunner.java
+++ src/test/java/TestRunners/ShopTestRunner.java
@@ -7,41 +7,41 @@ import org.testng.annotations.Test;
 
 public class ShopTestRunner extends Setup {
 
-  @Test(priority = 0,groups = "purchase",enabled = false)
+  @Test(priority = 0,enabled = false)
     public void visitShopTab(){
         driver.get("https://envothemes.com/envo-ecommerce");
         Shop shop = new Shop(driver);
         shop.doClickShopTab();
     }
 
-    @Test(priority = 1, groups = "purchase",enabled = false)
+    @Test(priority = 1,enabled = false)
     public void sortProducts(){
         driver.get("https://envothemes.com/envo-ecommerce");
         Shop shop = new Shop(driver);
         shop.sortByPrice();
     }
-    @Test(priority = 2,groups = "purchase",enabled = false)
+    @Test(priority = 2,enabled = false)
     public void colorFilter(){
         driver.get("https://envothemes.com/envo-ecommerce");
         Shop shop = new Shop(driver);
         shop.searchByColor();
     }
 
-    @Test(priority = 3,groups = "purchase",enabled = false)
+    @Test(priority = 3,enabled = false)
     public void searchProduct(){
         driver.get("https://envothemes.com/envo-ecommerce");
         Shop shop = new Shop(driver);
         shop.searchByName();
     }
 
-    @Test(priority = 4,groups = "purchase",enabled = false)
+    @Test(priority = 4,enabled = false)
     public void productNotFound(){
         driver.get("https://envothemes.com/envo-ecommerce");
         Shop shop = new Shop(driver);
         shop.searchByWrongName();
     }
 
-    @Test(priority = 5,groups = "purchase")
+    @Test(priority = 5)
     public void selectProduct(){
         driver.get("https://envothemes.com/envo-ecommerce");
         Shop shop = new Shop(driver);
